f44a8cb4065a73f86bba64315d24ca3944ddf6e6
Edward John Steere <edward.steere@gmail.com>
2019-01-20 14:15:30 +0200

    Support debugging via build script flag

--- build.sh
+++ build.sh
@@ -10,6 +10,12 @@ INCLUDE=-I"/usr/include/SDL2/"
 
 LINK=-l"SDL2-2.0"
 
+EXTRAS=
+
+if [ "$1" == "debug" ]; then
+    EXTRAS="-g $EXTRAS"
+fi
+
 pushd $BUILD_DIR
-g++ ../src/handmade.cpp $INCLUDE $LINK -o ../$BUILD_DIR/handmade
+g++ ../src/handmade.cpp $EXTRAS $INCLUDE $LINK -o ../$BUILD_DIR/handmade
 popd
