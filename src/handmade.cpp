c1959382ba05a753e57c57c144c5ac519182f4ec
Edward John Steere <edward.steere@gmail.com>
2019-01-20 14:01:02 +0200

    Create a window in the dummy entry point

--- src/handmade.cpp
+++ src/handmade.cpp
@@ -1,5 +1,18 @@
+#include <time.h>
+
 #include "SDL.h"
 
 int main (int argc, char** argv) {
-    return 0;
+    const auto window = SDL_CreateWindow("Hello world!",
+                                         0,
+                                         0,
+                                         200,
+                                         300,
+                                         0);
+    while (true) {
+        const auto duration = new timespec();
+        duration->tv_sec  = 1;
+        duration->tv_nsec = 0;
+        nanosleep(duration, NULL);
+    };
 }
