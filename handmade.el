f194e9736d45f51544e4c4217ae73b904d7e3775
Edward John Steere <edward.steere@gmail.com>
2019-01-20 14:15:51 +0200

    Add a file with convenience functions for working on this project

---
+++ handmade.el
@@ -0,0 +1,17 @@
+;;; handmade --- A collection of functions for editing this project -*- lexical-binding: t; -*-
+
+;;; Commentary:
+
+;;; Code:
+
+(require 'project)
+
+(defun handmade-debug ()
+  "Run and debug handmade."
+  (interactive)
+  (let* ((root-dir          (car (project-roots (project-current))))
+         (default-directory root-dir))
+    (gdb (format "gdb -i=mi %sbin/handmade" root-dir))))
+
+(provide 'handmade)
+;;; handmade ends here
