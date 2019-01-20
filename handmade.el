e4bb5f9ee0681af75edd66851f50f9f1472033ba
Edward John Steere <edward.steere@gmail.com>
2019-01-20 14:22:11 +0200

    Debug with many windows

--- handmade.el
+++ handmade.el
@@ -11,7 +11,8 @@
   (interactive)
   (let* ((root-dir          (car (project-roots (project-current))))
          (default-directory root-dir))
-    (gdb (format "gdb -i=mi %sbin/handmade" root-dir))))
+    (gdb (format "gdb -i=mi %sbin/handmade" root-dir))
+    (gdb-many-windows)))
 
 (provide 'handmade)
 ;;; handmade ends here
