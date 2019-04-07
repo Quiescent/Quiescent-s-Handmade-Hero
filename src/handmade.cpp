88e21b527362ee21bea306513afbce8ebafbc7dc
Edward Steere <edward.steere@gmail.com>
2019-04-07 15:49:54 +0000

    Include window event handling

--- src/handmade.cpp
+++ src/handmade.cpp
@@ -1,7 +1,33 @@
 #include <time.h>
 #include <SDL.h>
+#include <functional>
 
-int main (int argc, char** argv) {
+typedef std::function<void(void)> Thunk;
+
+void handleWindowEvent(const SDL_Event* event, const Thunk exitProgram) {
+    switch (event->window.event) {
+    case SDL_WINDOWEVENT_RESIZED:
+        break;
+    case SDL_WINDOWEVENT_SIZE_CHANGED:
+        break;
+    case SDL_WINDOWEVENT_CLOSE:
+        exitProgram();
+        break;
+    default:
+        break;
+    }
+}
+
+void handleWindowEvent(const Thunk exitProgram) {
+    SDL_Event event;
+    if (SDL_PollEvent(&event) && event.window.event) {
+        handleWindowEvent(exitProgram);
+    }
+}
+
+static const Thunk exitProgram = []() { exit(0); };
+
+int main(int argc, char** argv) {
     const auto window = SDL_CreateWindow("Hello world!",
                                          0,
                                          0,
@@ -13,5 +39,6 @@ int main (int argc, char** argv) {
         duration->tv_sec  = 1;
         duration->tv_nsec = 0;
         nanosleep(duration, NULL);
+        handleWindowEvent(exitProgram);
     };
 }
